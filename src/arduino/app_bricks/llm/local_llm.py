1c03887998b6e06d08a40ce7fc396d8775a4fbbf
Roberto Gazia <r.gazia@arduino.cc>
2026-05-27 12:10:09 +0200

    Add llama.cpp support (#244)

--- src/arduino/app_bricks/llm/local_llm.py
+++ src/arduino/app_bricks/llm/local_llm.py
@@ -94,9 +94,9 @@ class LargeLanguageModel(CloudLLM):
             if model.startswith(self.GENIE_MODEL):
                 port = 9001
                 host = "genie-models-runner"
-            # elif model.startswith(self.LLAMACPP_MODEL):
-            #     port = 9999
-            #     host = "llamacpp-models-runner"
+            elif model.startswith(self.LLAMACPP_MODEL):
+                port = 9999
+                host = "llamacpp-models-runner"
             # elif model.startswith(self.OLLAMA_MODEL):
             #     port = 11434
             #     host = "ollama-models-runner"
